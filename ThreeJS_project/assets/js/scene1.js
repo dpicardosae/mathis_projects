be490b69379eef504a130a8e1f5213cd3dbffeb9
LM9 <lamamat91@gmail.com>
2022-11-13 12:04:30 +0100

    Modification taille lignes + ajout vec()

--- ThreeJS_project/assets/js/scene1.js
+++ ThreeJS_project/assets/js/scene1.js
@@ -4,9 +4,10 @@ function deployS1() {
 }
 
 function sceneObjects() {
-    new GameObject("Box", [1, 500, 1], v0, v0, 0x00ff00);
-    new GameObject("Line", [v0, new THREE.Vector3(100,0,0)], v0, v0, 0xff0000);
-    new GameObject("Line", [v0, new THREE.Vector3(0,0,100)], v0, v0, 0x0000ff);
+    axisLen = 200;
+    new GameObject("Box", [1, axisLen, 1], vec(0,axisLen/2,0), v0, 0x00ff00);
+    new GameObject("Box", [axisLen, 1, 1], vec(axisLen/2, 0, 0), v0, 0xff0000);
+    new GameObject("Box", [1,1,axisLen], vec(0, 0, axisLen/2), v0, 0x0000ff);
 
     new GameObject("Box", [50,50,50], v0, v0, 0xffa500);
 }
@@ -27,8 +28,8 @@ function islandFloor() {
             sols.push(new GameObject(
                 "Plane",
                 [floorWH,floorWH],
-                new THREE.Vector3(floorWH * j - currSolW, 0, floorWH * i - currSolD),
-                new THREE.Vector3(Math.PI / 2., 0, 0) // 90°
+                vec(floorWH * j - currSolW, 0, floorWH * i - currSolD),
+                vec(Math.PI / 2., 0, 0) // 90°
             ));
         }
     }
---
+++ ThreeJS_project/assets/js/tools.js
@@ -0,0 +1,3 @@
+function vec(x=0,y=0,z=0) {
+    return new THREE.Vector3(x,y,z);
+}
