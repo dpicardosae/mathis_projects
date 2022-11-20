75371a5e587621643be5e2bda7042834faa1647e
LM9 <lamamat91@gmail.com>
2022-11-20 17:40:59 +0100

    Forgot to "git add" on previous commit causing missing settings.js and TODO.txt

---
+++ ThreeJS_project/assets/js/Settings.js
@@ -0,0 +1,14 @@
+class Settings {
+    constructor() {
+        this.debugAxisLen = 200;
+        this.defPlatformDims = vec(500, 50, 500);
+        this.lavaDims = [10000, 2000];
+
+        this.playerHeight = 50;
+        this.jumpHeight = 1000;
+        this.gravity = 1500;
+
+        this.playerRotSpeed = 1;
+        this.playerMoveSpeed = 600;
+    }
+}
