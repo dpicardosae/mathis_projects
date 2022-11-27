7ef4e23732ef5ac6e25d611e0ed4a9cae0164529
dpicard <davidpa.picard@gmail.com>
2022-11-27 12:07:29 +0100

    Update script.py

--- Python/script.py
+++ Python/script.py
@@ -3,18 +3,19 @@ import math
 
 x_list = []
 y_list = []
+y_sqrt = []
 y2_list = []
 
-n = 10.0
+n = 30
 
 sum = 0
 
 for i in range(1, int(n+1)):
 	x_list.append(i)
-	sum += 1/i
+	sum += 1./i
 	y_list.append(sum)
-	y2_list.append(1.0-math.pow((n-1.0)/n, n))
+	y_sqrt.append(math.sqrt(i/10.))
+	y2_list.append(1.0-math.pow((i-1.0)/i, i))
 
 plt.plot(x_list, y2_list)
-print(y2_list)
 plt.show()
